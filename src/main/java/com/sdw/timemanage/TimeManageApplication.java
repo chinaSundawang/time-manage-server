0266911e9e92784a53ddae7e2e700c4a427bc704
sundawang <sundawang@bz365.com>
2022-10-14 14:38:54 +0800

    first commit

---
+++ src/main/java/com/sdw/timemanage/TimeManageApplication.java
@@ -0,0 +1,13 @@
+package com.sdw.timemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdw/timemanage/TimeManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdw.timemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
